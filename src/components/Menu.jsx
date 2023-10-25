7b2274989b6fc82f721fbfd4ea3907907bf39a8a
requenahdz <requenahdz@gmail.com>
2023-10-24 22:41:41 -0600

    fix menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,25 @@
 import { Menubar } from "primereact/menubar";
+import { useNavigate } from "react-router-dom";
+
 export default function Menu() {
+  const navigate = useNavigate();
   const items = [
     {
       label: "Dashboard",
       icon: "pi pi-chart-bar",
+      command: () => {navigate('/') }
     },
 
     {
       label: "Tareas",
       icon: "pi pi- pi-list",
+      command: () => {navigate('/tasks') }
     },
     {
-        label: "Categorias",
-        icon: "pi pi- pi-check-square",
-      },
+      label: "Categorias",
+      icon: "pi pi- pi-check-square",
+      command: () => {navigate('/categories') }
+    },
   ];
 
   const start = (
@@ -25,5 +31,5 @@ export default function Menu() {
     ></img>
   );
 
-  return <Menubar model={items} start={start} />;
+  return <Menubar  model={items} start={start} />;
 }
--- src/modules/tasks/new.jsx
+++ src/modules/tasks/new.jsx
@@ -5,70 +5,12 @@ const TaskNew = () => {
     <div>
       <h1 className="text-3xl">Nueva tarea</h1>
 
-      <div className="w-full max-w-screen-sm mx-auto">
-        <div className="w-full max-w-screen-sm mx-auto">
-          <div className="grid grid-cols-2 gap-4">
-            <div>
-              <label
-                htmlFor="nombre"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Nombre
-              </label>
-              <InputText
-                id="nombre"
-                className="mt-1 p-inputtext"
-                placeholder="Escribe tu nombre"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="apellido"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Apellido
-              </label>
-              <InputText
-                id="apellido"
-                className="mt-1 p-inputtext"
-                placeholder="Escribe tu apellido"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="correo"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Correo Electrónico
-              </label>
-              <InputText
-                id="correo"
-                className="mt-1 p-inputtext"
-                placeholder="Escribe tu correo electrónico"
-              />
-            </div>
-            <div>
-              <label
-                htmlFor="telefono"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Teléfono
-              </label>
-              <InputText
-                id="telefono"
-                className="mt-1 p-inputtext"
-                placeholder="Escribe tu número de teléfono"
-              />
-            </div>
-          </div>
-          <div className="mt-4">
-            <Button
-              label="Enviar"
-         
-            />
-          </div>
-        </div>
+      <div className="flex-col">
+        <label htmlFor="name" >Username</label>
+        <InputText name="name" placeholder="Nombre" />
       </div>
+
+
     </div>
   );
 };
